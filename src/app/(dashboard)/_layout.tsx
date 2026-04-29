235504ccaf33f366cabd7dc4a4ec2c1cdda9d04f
jehovah-son <chukwukasuccess0@gmail.com>
2026-04-29 19:49:15 +0100

    `Updated app.json and package.json to change UI style to dark and added @expo/vector-icons dependency`

--- src/app/(dashboard)/_layout.tsx
+++ src/app/(dashboard)/_layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Tabs } from "expo-router";
 import { useColorScheme } from "react-native";
 import { Colors } from "../../../constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function DashBoardLayout() {
   const colorScheme = useColorScheme();
@@ -19,9 +20,54 @@ export default function DashBoardLayout() {
         tabBarInactiveTintColor: themeColors.iconColor,
       }}
     >
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
-      <Tabs.Screen name="books" options={{ title: "Books" }} />
-      <Tabs.Screen name="create" options={{ title: "Create" }} />
+      {/* person tabs */}
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              size={24}
+              name={focused ? "person" : "person-outline"}
+              color={
+                focused ? themeColors.iconColorFocused : themeColors.iconColor
+              }
+            />
+          ),
+        }}
+      />
+      {/* books tab */}
+      <Tabs.Screen
+        name="books"
+        options={{
+          title: "Books",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              size={24}
+              name={focused ? "book" : "book-outline"}
+              color={
+                focused ? themeColors.iconColorFocused : themeColors.iconColor
+              }
+            />
+          ),
+        }}
+      />
+      {/* create tab */}
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              size={24}
+              name={focused ? "create" : "create-outline"}
+              color={
+                focused ? themeColors.iconColorFocused : themeColors.iconColor
+              }
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
