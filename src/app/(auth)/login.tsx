81e997b190e5a3e9a765d180773c5a9561928c83
jehovah-son <chukwukasuccess0@gmail.com>
2026-04-30 15:28:44 +0100

    `Added Keyboard.dismiss to prevent keyboard from opening on touch outside of input fields in login and register screens`

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -5,46 +5,47 @@ import { Link, Stack } from "expo-router";
 
 import ThemeTextInput from "@/components/ThemeTextInput";
 import { useState } from "react";
-
-const handleSubmit = () => {
-  console.log("login submitted");
-};
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [passWord, setPassWord] = useState("");
 
+  const handleSubmit = () => {
+    console.log("login submitted", email, passWord);
+  };
   return (
-    <ThemedView className="container mx-auto flex-1 justify-center items-center">
-      <ThemedText title={true} className="text-xl font-bold">
-        Login to your account
-      </ThemedText>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <ThemedView className="container mx-auto flex-1 justify-center items-center">
+        <ThemedText title={true} className="text-xl font-bold">
+          Login to your account
+        </ThemedText>
 
-      <ThemeTextInput
-        placeholder="Email"
-        style={{ width: "80%", marginBottom: 20 }}
-        className="mt-5"
-        keyBoardType="email-address"
-        onChangeText={setEmail}
-        value={email}
-      />
-      <ThemeTextInput
-        placeholder="Password"
-        style={{ width: "80%", marginBottom: 20 }}
-        className="mt-5"
-        keyBoardType="password"
-        onChangeText={setPassWord}
-        value={passWord}
-        secureTextEntry
-      />
+        <ThemeTextInput
+          placeholder="Email"
+          style={{ width: "80%", marginBottom: 20 }}
+          className="mt-5"
+          keyBoardType="email-address"
+          onChangeText={setEmail}
+          value={email}
+        />
+        <ThemeTextInput
+          placeholder="Password"
+          style={{ width: "80%", marginBottom: 20 }}
+          className="mt-5"
+          onChangeText={setPassWord}
+          value={passWord}
+          secureTextEntry
+        />
 
-      <ThemeButtonProps text="Login" onPress={handleSubmit} />
-      <Link
-        href="/register"
-        className="mt-5 text-lg font-bold underline text-center"
-      >
-        <ThemedText> Register your account</ThemedText>
-      </Link>
-    </ThemedView>
+        <ThemeButtonProps text="Login" onPress={handleSubmit} />
+        <Link
+          href="/register"
+          className="mt-5 text-lg font-bold underline text-center"
+        >
+          <ThemedText> Register your account</ThemedText>
+        </Link>
+      </ThemedView>
+    </TouchableWithoutFeedback>
   );
 }
--- src/app/(auth)/register.tsx
+++ src/app/(auth)/register.tsx
@@ -3,49 +3,55 @@ import ThemedText from "@/components/ThemeText";
 import { Link, Stack } from "expo-router";
 import React, { useState } from "react";
 import ThemeButtonProps from "@/components/ThemeButton";
-import { TextInput, View } from "react-native";
+import {
+  Keyboard,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import ThemeTextInput from "@/components/ThemeTextInput";
 
-const handleSubmit = () => {
-  console.log("register submitted");
-};
-
 export default function Register() {
+  const handleSubmit = () => {
+    console.log("register submitted", email, passWord);
+  };
+
   const [email, setEmail] = useState("");
   const [passWord, setPassWord] = useState("");
 
   return (
-    <ThemedView className="container mx-auto flex-1 justify-center items-center">
-      <ThemedText title={true} className="text-xl font-bold">
-        Register to your account
-      </ThemedText>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <ThemedView className="container mx-auto flex-1 justify-center items-center">
+        <ThemedText title={true} className="text-xl font-bold">
+          Register to your account
+        </ThemedText>
 
-      <ThemeTextInput
-        placeholder="Email"
-        style={{ width: "80%", marginBottom: 20 }}
-        className="mt-5"
-        keyBoardType="email-address"
-        onChangeText={setEmail}
-        value={email}
-      />
-      <ThemeTextInput
-        placeholder="Password"
-        style={{ width: "80%", marginBottom: 20 }}
-        className="mt-5"
-        keyBoardType="password"
-        onChangeText={setPassWord}
-        value={passWord}
-        secureTextEntry
-      />
+        <ThemeTextInput
+          placeholder="Email"
+          style={{ width: "80%", marginBottom: 20 }}
+          className="mt-5"
+          keyBoardType="email-address"
+          onChangeText={setEmail}
+          value={email}
+        />
+        <ThemeTextInput
+          placeholder="Password"
+          style={{ width: "80%", marginBottom: 20 }}
+          className="mt-5"
+          onChangeText={setPassWord}
+          value={passWord}
+          secureTextEntry
+        />
 
-      <ThemeButtonProps text="Register" onPress={handleSubmit} />
+        <ThemeButtonProps text="Register" onPress={handleSubmit} />
 
-      <Link
-        href="/login"
-        className="mt-5 text-lg font-bold underline text-center"
-      >
-        <ThemedText> already have an account? Login here</ThemedText>
-      </Link>
-    </ThemedView>
+        <Link
+          href="/login"
+          className="mt-5 text-lg font-bold underline text-center"
+        >
+          <ThemedText> already have an account? Login here</ThemedText>
+        </Link>
+      </ThemedView>
+    </TouchableWithoutFeedback>
   );
 }
